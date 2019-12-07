ea481fcbbb9dd30d539809dc3d43be3e311ed670
Tom MOUNET <Mister0x@protonmail.com>
2019-12-07 17:38:47 +0100

    Beautify and clarify server

--- server.py
+++ server.py
@@ -11,28 +11,32 @@ import socket
 import configparser
 from hashlib import sha224
 
+# Read configuration
 CONFIG_FILE = "sweet_onions.cfg"
 
 config = configparser.ConfigParser()
 config.read(CONFIG_FILE)
 
-IP = socket.gethostbyname(socket.gethostname())
-PORT = int(config['DEFAULT']['Port'])
-BUFFER_SIZE = int(config['SERVER']['BufferSize'])
+IP: str = socket.gethostbyname(socket.gethostname())
+PORT: int = int(config["DEFAULT"]["Port"])
+BUFFER_SIZE: int = int(config["SERVER"]["BufferSize"])
 
+# Open socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((IP, PORT))
 s.listen(1) #maximum 1 connection
+
 print("Connection opened on {}:{}".format(IP,PORT))
 
-while 1:
+# Return sha224 of first data for each connections
+data = 'useless garbage'
+while data:
 	conn, addr = s.accept()
-	addr = addr[0]
-	print('Connection from {}'.format(addr))
+	print("Connection from {}".format(addr[0]))
 	data = conn.recv(BUFFER_SIZE)
 	hashed = sha224(data).hexdigest()
-	if not data: break
-	print("Received : {}".format(data))
+	#if not data: break
+	print("Received : {}".format(data.decode().split()))
 	conn.send(bytes(hashed + '\n',"utf-8"))
 
 conn.close()
