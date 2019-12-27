b534a78f6951ac9704ed4a4eb2da7fe0ef60cacf
MaitreRenard <tom.mounet@protonmail.com>
2019-12-27 16:27:17 +0100

    Renaming for a more efficient launching/building script

--- launch.sh
+++ launch.sh
@@ -1,19 +1,28 @@
 #!/usr/bin/env bash
 
+DOCKER_OPTS="-it --rm --network host"
+
+# $1 : The info to print
 info() {
 	printf '\n\e[1;32m--------------------------------\n'
 	printf '[INFO]	%s\n' "$1"
 	printf -- "--------------------------------\e[m\n\n"
 }
 
-info "BUILDING DIRECTORY CONTAINER"
-docker build -t sweet_onions/directory	-f docker/Directory .
+# $1 : Dockerfile's name in "docker" dir in PascalCase
+build_container() {
+	lower_name=$(echo "$1" |tr "[:upper:]" "[:lower:]")
+	upper_name=$(echo "$1" |tr "[:lower:]" "[:upper:]")
 
-info "BUILDING NODE CONTAINER"
-docker build -t sweet_onions/node		-f docker/Node .
+	info "BUILDING $upper_name CONTAINER"
+	docker build -t sweet_onions/$lower_name -f docker/$1 .
+}
 
-info "BUILDING SERVER CONTAINER"
-docker build -t sweet_onions/server		-f docker/Server .
+
+build_container Generic
+build_container Directory
+build_container Node
+build_container Server
 
 info "STARTING DIRECTORY"
-docker run -it --rm --network host sweet_onions/directory
+docker run $DOCKER_OPTS sweet_onions/directory
