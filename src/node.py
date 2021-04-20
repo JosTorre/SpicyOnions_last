9a95cc23f11986c310807224a2db548c3a018eff
Zé Torre <torre.avilais@gmail.com>
2021-04-20 11:36:10 +0200

    reinfolge

--- src/node.py
+++ src/node.py
@@ -168,6 +168,34 @@ def threaded_client(back):
         proceed = process(cell)
 >>>>>>> 25b401430924ee04a0dbd6770777d3920f1765ac
 
+#Circuit Creation
+# ----------------------------------------------------------------
+
+def threaded_client(back):
+    print("threaded_client")
+    proceed = True
+
+    while proceed:
+        data = back.recv(2048) # We get data from predecesor
+        cell = pickle.loads(data)
+
+        #Check keys
+        if cell.hlen == 32 :
+            peer_public = x25519.X25519PublicKey.from_public_bytes(cell.hdata)
+            shared_onion_key = private_onion_key.exchange(peer_public)
+            derived_key = HKDF(
+                algorithm=hashes.SHA256(),
+                length=32,
+                salt=None,
+                info=b'handshake data',
+                backend=backend
+            ).derive(shared_onion_key)
+
+        print('Shared Secret:')
+        print(derived_key)
+
+        proceed = process(cell)
+
 #Functions
 # ----------------------------------------------------------------
 
@@ -305,32 +333,6 @@ def forward(cell):
     front.send(pickled_cell)
 <<<<<<< HEAD
 
-#Circuit Creation
-# ----------------------------------------------------------------
 
-def threaded_client(back):
-    print("threaded_client")
-    proceed = True
-
-    while proceed:
-        data = back.recv(2048) # We get data from predecesor
-        cell = pickle.loads(data)
-
-        #Check keys
-        if cell.hlen == 32 :
-            peer_public = x25519.X25519PublicKey.from_public_bytes(cell.hdata)
-            shared_onion_key = private_onion_key.exchange(peer_public)
-            derived_key = HKDF(
-                algorithm=hashes.SHA256(),
-                length=32,
-                salt=None,
-                info=b'handshake data',
-                backend=backend
-            ).derive(shared_onion_key)
-
-        print('Shared Secret:')
-        print(derived_key)
-
-        proceed = process(cell)
 =======
 >>>>>>> 25b401430924ee04a0dbd6770777d3920f1765ac
